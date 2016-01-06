2ccf65a6ce1bfe7066483c8a4c3625f6c9b35a12
Adrian Oesch <adrianoesch@gmx.ch>
2016-01-06 23:16:51 +0100

    new user functionality

---
+++ userInfo.js
@@ -0,0 +1,8 @@
+userInfo = [{
+		"UID": "adrian_03",
+		"ResponseID": "R_2gFgUAR4obTGygZ",
+		"mefirst": 0,
+		"colorcode": "0214563",
+		"arrowcode": "0010111",
+		"charity": "Declaration of Bern"
+	}]
