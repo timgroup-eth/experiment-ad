8f351d1d87e2dde674a551e1a2a253f176b17143
Adrian Oesch <adrianoesch@gmx.ch>
2016-01-30 07:07:08 +0100

    change redirect to qualtrics text

--- instructions.js
+++ instructions.js
@@ -10,6 +10,6 @@ var instructions = {
   end_fullscreen : '<h2>This was it!</h2> <p>Please save your data by clicking "Save & Exit". You\'ll get your\
                    confirmation code on the next page.</p>',
 
-  confirmation : '<h2>This session is over</h2><p>Please go back to the survey form and paste the following code into the\
+  confirmation : '<h2>This session is over</h2><p>Please go back to the qualtrics survey and paste the following code into the\
                 input form: <strong><span id="conf_code"></span></strong></p><p>After copying the confirmation code, you can close this browser window or tab.</p>'
 };
