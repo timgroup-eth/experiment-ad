2752fc3f3cade73d8cde1b4f334d883da4716152
Adrian Oesch <adrianoesch@gmx.ch>
2016-02-02 18:11:04 +0100

    additional instructions

--- instructions.js
+++ instructions.js
@@ -5,11 +5,13 @@ var instructions = {
               <li>reloading the webpage</li><li>going-"back" in the browser</li></ul>\
               <p>Please close all other running applications and turn off all notifications before entering \
               the experiment. Ensure you are in a calm place without any distractions, for example, noise.<p>\
+              <p>If your session is aborted, you can restart it reopening the link from qualtrics.</p>\
               <p>Click "Enter" to launch into fullscreen and start the experiment.',
 
-  end_fullscreen : '<h2>This was it!</h2> <p>Please save your data by clicking "Save & Exit". You\'ll get your\
+  end_fullscreen : '<h2>This was it!</h2> <p>Please save your data by clicking "Save & Exit". You will get your\
                    confirmation code on the next page.</p>',
 
   confirmation : '<h2>This session is over</h2><p>Please go back to the qualtrics survey and paste the following code into the\
-                input form: <strong><span id="conf_code"></span></strong></p><p>After copying the confirmation code, you can close this browser window or tab.</p>'
+                input form <br>(use right-click in Safari): <strong><span id="conf_code"></span></strong></p>\
+                <p>After copying the confirmation code, you can close this browser window or tab.</p>'
 };
