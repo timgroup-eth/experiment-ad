836538a67a19f5489f4fb052d719907ad348981b
Adrian Oesch <adrianoesch@gmx.ch>
2016-03-16 15:18:45 +0100

    fix mailgun

--- mailgun/mailgun.php
+++ mailgun/mailgun.php
@@ -1,4 +1,6 @@
 <?php
+$to = $_POST['to'];
+$deliveryTime = $_POST['deliveryTime'];
 
 # Include the Autoloader (see "Libraries" for install instructions)
 require 'vendor/autoload.php';
@@ -11,7 +13,8 @@ $domain = "sandboxf301d7cdb4cf43e5b9c63425d90850dd.mailgun.org";
 # Make the call to the client.
 $result = $mgClient->sendMessage("$domain",
                   array('from'    => 'Mailgun Sandbox <postmaster@sandboxf301d7cdb4cf43e5b9c63425d90850dd.mailgun.org>',
-                        'to'      => 'Adrian Oesch <oescha@ethz.ch>',
-                        'subject' => 'Hello Adrian Oesch',
-                        'text'    => 'Congratulations Adrian Oesch, you just sent an email with Mailgun!  You are truly awesome!  You can see a record of this email in your logs: https://mailgun.com/cp/log .  You can send up to 300 emails/day from this sandbox server.  Next, you should add your own domain so you can send 10,000 emails/month for free.'));
+                        'to'      => $to,
+                        'subject' => 'Timgroup Experiment Notification',
+                        'text'    => 'Hi! \n\n The next session of the experiment is now available. Please reopen the link from qualtrics and continue with the next task. \n\n Best regards, the notification bot.',
+                        'o:delivertime' => $deliveryTime ));
 ?>
