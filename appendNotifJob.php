a60fd02f61671b94cfacbb31d225114fdb9547cc
Adrian Oesch <adrianoesch@gmx.ch>
2016-03-16 20:00:34 +0100

    fix append

--- appendNotifJob.php
+++ appendNotifJob.php
@@ -2,5 +2,5 @@
 $openshift_data_dir = $_ENV["OPENSHIFT_DATA_DIR"];
 $notifFile = $openshift_data_dir.'notifications.txt';
 $notifStr = file_get_contents($notifFile);
-file_put_contents( $notifStr . $_POST['notifJob']);
+file_put_contents( $notifFile, $notifStr.$_POST['notifJob']);
 ?>;
