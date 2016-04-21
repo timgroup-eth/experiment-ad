d687c8b8fe2c701d524e74dcfa478ab091d737a1
Adrian Oesch <adrianoesch@gmx.ch>
2016-04-21 14:41:28 +0200

    fix typo

--- instructions.js
+++ instructions.js
@@ -6,7 +6,7 @@ var instructions = {
               <p>Please close all other running applications and turn off all notifications before entering \
               the experiment. Ensure you are in a calm place without any distractions, for example, noise.<p>\
               <p>If your session is aborted, you can start again reopening the link from qualtrics.</p>\
-              <p>Click "Enter" to launch into fullscreen and start the experiment.',
+              <p>Click "Enter" to launch into fullscreen and start the experiment.</p>',
 
   end_fullscreen : '<h2>This was it!</h2> <p>Please save your data by clicking "Save & Exit". You will get your\
                    confirmation code on the next page.</p>',
